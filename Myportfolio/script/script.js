b1867b9d032fdee2c469be0b820b095677646635
ahamie71 <101115517+ahamie71@users.noreply.github.com>
2022-11-25 13:36:47 +0100

    Add files via upload

---
+++ Myportfolio/script/script.js
@@ -0,0 +1,22 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
+var lineDrawing = anime({
+    targets: '#lineDrawing .lines path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 3000,
+    delay: function(el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true
+  });
